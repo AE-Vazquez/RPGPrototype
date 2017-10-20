9614151410eca5b69be0c58023df714cda1ca679
Eduardo_Vazquez <eduardo.vazquezn@gmail.com>
2017-10-20 19:41:54 +0300

    Added Standard Assets. Added fixed camera follow script

---
+++ Dank Souls/Assets/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    private Transform _mTarget;
+
+
+	void Start () {
+        GameObject playerGO = GameObject.FindGameObjectWithTag("Player");
+
+        if (playerGO != null)
+            _mTarget = playerGO.transform;
+	}
+	
+
+	void LateUpdate () {
+        if (_mTarget != null)
+            transform.position = _mTarget.position;
+	}
+}
