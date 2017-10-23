a867b8ede1fd8eb4e826f7df3bc2df41b90809f2
Eduardo_Vazquez <eduardo.vazquezn@gmail.com>
2017-10-23 18:14:59 +0300

    Updated PlayerMovement to toggle between Mouse or Direction Keys

--- Dank Souls/Assets/Player/PlayerMovement.cs
+++ Dank Souls/Assets/Player/PlayerMovement.cs
@@ -5,12 +5,15 @@ using UnityStandardAssets.Characters.ThirdPerson;
 [RequireComponent(typeof (ThirdPersonCharacter))]
 public class PlayerMovement : MonoBehaviour
 {
+   
     [SerializeField]
     float m_minMoveDistance = 0.2f; //Minimum distance required to make the character move
     ThirdPersonCharacter m_Character;   // A reference to the ThirdPersonCharacter on the object
     CameraRaycaster cameraRaycaster;
     Vector3 currentClickTarget;
-        
+
+    bool m_isInDirectMode = false;
+
     private void Start()
     {
         cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
@@ -21,10 +24,51 @@ public class PlayerMovement : MonoBehaviour
     // Fixed update is called in sync with physics
     private void FixedUpdate()
     {
+        if(Input.GetKeyDown(KeyCode.G))
+        {
+            m_isInDirectMode = !m_isInDirectMode;
+
+        }
+
+        if (m_isInDirectMode)
+        {
+            ProcessDirectMovement();
+        }
+        else
+        {
+            ProcessMouseMovement();
+        }
+    }
+
+    private void ProcessDirectMovement()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        // calculate move direction to pass to character
+        Vector3 m_Move;
+        if (Camera.main != null)
+        {
+            Transform m_CamTransform = Camera.main.transform;
+            // calculate camera relative direction to move:
+            Vector3 m_CamForward = Vector3.Scale(m_CamTransform.forward, new Vector3(1, 0, 1)).normalized;
+            m_Move = v * m_CamForward + h * m_CamTransform.right;
+        }
+        else
+        {
+            // we use world-relative directions in the case of no main camera
+            m_Move = v * Vector3.forward + h * Vector3.right;
+        }
+
+        m_Character.Move(m_Move, false, false);
+    }
+
+    private void ProcessMouseMovement()
+    {
         if (Input.GetMouseButton(0))
         {
 
-            switch(cameraRaycaster.layerHit)
+            switch (cameraRaycaster.layerHit)
             {
                 case Layers.Walkable:
                     currentClickTarget = cameraRaycaster.hit.point;
@@ -33,6 +77,7 @@ public class PlayerMovement : MonoBehaviour
                     Debug.Log("ATTACK ENEMY!");
                     break;
                 default:
+                    currentClickTarget = transform.position;
                     break;
             }
 
@@ -42,6 +87,10 @@ public class PlayerMovement : MonoBehaviour
         if (moveVector.magnitude >= m_minMoveDistance)
         {
             m_Character.Move(moveVector, false, false);
+        }
+        else
+        {
+            m_Character.Move(Vector3.zero,false,false);
         }
     }
 }
